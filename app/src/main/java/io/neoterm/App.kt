bf510e9e0acffe7c123d093e2f1986cd3cfda148
zt515 <kiva515@foxmail.com>
2017-08-15 19:22:04 +0800

    Fix: Show help

--- app/src/main/java/io/neoterm/App.kt
+++ app/src/main/java/io/neoterm/App.kt
@@ -40,7 +40,9 @@ class App : Application() {
     }
 
     fun openHelpLink() {
-        startActivity(Intent(Intent.ACTION_VIEW, Uri.parse("https://neoterm.gitbooks.io/neoterm-wiki/content/")))
+        val intent = Intent(Intent.ACTION_VIEW, Uri.parse("https://neoterm.gitbooks.io/neoterm-wiki/content/"))
+        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+        startActivity(intent)
     }
 
     companion object {

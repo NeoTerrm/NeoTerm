fbe04df42ea89d5979a42649ab28031ea52029f2
zt515 <kiva515@foxmail.com>
2017-11-03 23:43:24 +0800

    Improve: better color preview

--- app/src/main/java/io/neoterm/ui/customize/adapter/holder/ColorItemViewHolder.kt
+++ app/src/main/java/io/neoterm/ui/customize/adapter/holder/ColorItemViewHolder.kt
@@ -11,13 +11,16 @@ import io.neoterm.ui.customize.model.ColorItem
 class ColorItemViewHolder(private val rootView: View, private val listener: ColorItemAdapter.Listener) : SortedListAdapter.ViewHolder<ColorItem>(rootView) {
     private val colorItemName: TextView = rootView.findViewById<TextView>(R.id.color_item_name)
     private val colorItemDesc: TextView = rootView.findViewById<TextView>(R.id.color_item_description)
+    private val colorView: View = rootView.findViewById<View>(R.id.color_item_view)
 
     override fun performBind(item: ColorItem) {
         rootView.setOnClickListener { listener.onModelClicked(item) }
         colorItemName.text = item.colorName
         colorItemDesc.text = item.colorValue
         if (item.colorValue.isNotEmpty()) {
-            colorItemDesc.setTextColor(TerminalColors.parse(item.colorValue))
+            val color = TerminalColors.parse(item.colorValue)
+            colorView.setBackgroundColor(color)
+            colorItemDesc.setTextColor(color)
         }
     }
 }

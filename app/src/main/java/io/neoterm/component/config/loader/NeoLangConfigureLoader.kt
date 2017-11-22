483ebe6bfa1cf46ea19586ceab5316b99a689f11
Sam <coltonezekiel@gmail.com>
2017-11-22 13:24:15 +0530

    Update NeoLangConfigureLoader.kt

--- app/src/main/java/io/neoterm/component/config/loader/NeoLangConfigureLoader.kt
+++ app/src/main/java/io/neoterm/component/config/loader/NeoLangConfigureLoader.kt
@@ -1,11 +1,11 @@
-package io.neoterm.component.config.loader
+package com.termux.component.config.loader
 
-import io.neoterm.component.config.IConfigureFileLoader
-import io.neoterm.frontend.config.NeoConfigureFile
+import com.termux.component.config.IConfigureFileLoader
+import com.termux.frontend.config.NeoConfigureFile
 import java.io.File
 
 /**
- * @author kiva
+ * @author Sam
  */
 class NeoLangConfigureLoader(val configFile: File) : IConfigureFileLoader {
     override fun loadConfigure(): NeoConfigureFile? {

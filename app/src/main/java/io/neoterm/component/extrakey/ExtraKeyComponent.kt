4b5a0a6372afc5470a3d7ea981288d0e1ec96b6b
imkiva <libkernelpanic@gmail.com>
2018-03-25 00:24:04 +0800

    Fix: extra keys component does not load config files

--- app/src/main/java/io/neoterm/component/extrakey/ExtraKeyComponent.kt
+++ app/src/main/java/io/neoterm/component/extrakey/ExtraKeyComponent.kt
@@ -62,7 +62,7 @@ class ExtraKeyComponent : ConfigFileBasedComponent<NeoExtraKey>(NeoTermPath.EKS_
         extraKeys.clear()
         File(baseDir)
                 .listFiles(NEOLANG_FILTER)
-                .filterNot { it.absolutePath != NeoTermPath.EKS_DEFAULT_FILE }
+                .filter { it.absolutePath != NeoTermPath.EKS_DEFAULT_FILE }
                 .mapNotNull { this.loadConfigure(it) }
                 .forEach {
                     registerShortcutKeys(it)
